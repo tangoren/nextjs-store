b3e6a30456186eab2ddf8ef0e83a68dfb40e1ec1
Osman Tangoren <otangoren@gmail.com>
2024-03-25 17:28:55 +0300

    style: update slider width boxed to full

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Slider() {
   return (
-    <div class="container">
-      <div class="bg-gradient-to-r from-indigo-100 to-indigo-200 h-[480px] mt-8 rounded-lg flex flex-col justify-center items-start gap-6 px-16">
-        <div className="text-5xl font-bold text-transparent bg-gradient-to-r from-indigo-500 to-indigo-700 bg-clip-text">
+    <div class="bg-gradient-to-b from-slate-100 to-slate-300 h-[480px] flex">
+      <div class="container flex flex-col justify-center items-start gap-6">
+        <div className="text-5xl font-bold text-transparent bg-gradient-to-r from-indigo-400 to-indigo-600 bg-clip-text">
           New arrivals are here
         </div>
 
