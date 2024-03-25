36561c8f0bd8b2c2dfbd1e776e12f323d7834605
Osman Tangoren <otangoren@gmail.com>
2024-03-25 13:03:02 +0300

    feat: add topbar component, update body font

---
+++ src/components/Topbar.js
@@ -0,0 +1,9 @@
+import { Truck } from "lucide-react";
+
+export default function Topbar() {
+  return (
+    <div className="flex items-center justify-center gap-1 py-3 text-sm font-semibold text-center text-white bg-indigo-600">
+      <Truck /> Get free delivery on orders over $75
+    </div>
+  );
+}
