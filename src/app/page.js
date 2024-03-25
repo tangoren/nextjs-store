5d3eb08ccaad65b0312c074481aac358f61d6163
Osman Tangoren <otangoren@gmail.com>
2024-03-25 17:13:51 +0300

    feat: add slider component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Topbar from "../components/Topbar";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Topbar />
       <Navbar />
+      <Slider />
 
       <div style={{ height: 5000 }}>{/* temporary space for scrolling */}</div>
     </div>
