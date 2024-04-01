8afc064fe9a459ce10a4597796ee3d823cf51656
Osman Tangoren <otangoren@gmail.com>
2024-04-01 18:00:07 +0300

    feat: add sonner

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
+
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontBody = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${fontBody.className} antialiased`}>{children}</body>
+      <body className={`${fontBody.className} antialiased`}>
+        {children}
+        <Toaster closeButton />
+      </body>
     </html>
   );
 }
